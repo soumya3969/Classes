0360556742d72c6e27c57bf7279dc4bb10605f0b
soumya <117825720+soumya3969@users.noreply.github.com>
2023-05-04 10:34:29 +0530

    classes

---
+++ Soumya@3969/String_mathfunction/script.js
@@ -0,0 +1,11 @@
+// math function
+document.write("<strong><u>JavaScript Math Function</u></strong>")
+document.write("<br/> absolute= " +Math.abs(-5.6));
+document.write("<br/>ceil= "+Math.ceil(4.6));
+document.write("<br/>floor= "+Math.floor(4.6));
+document.write("<br/>round= "+Math.round(4.6));
+document.write("<br/>power= "+Math.pow(4,2));
+document.write("<br/>suare-root= "+Math.sqrt(4,2));
+document.write("<br/> max of(4,5,6,12,1)= "+Math.max(4,5,6,12,1));
+document.write("<br/>min of(4,5,6,12,1)= "+Math.min(4,5,6,12,1));
+document.write("<br/>random= "+Math.random());
